5ad6a29ce43353f46857b7c0a9269b3ef1b20f8e
leducphong21 <leducphong21@gmail.com>
2017-06-26 11:16:50 +0700

    commit auth

--- backend/controllers/DashboardController.php
+++ backend/controllers/DashboardController.php
@@ -5,9 +5,37 @@ namespace backend\controllers;
 
 
 use yii\web\Controller;
+use yii\filters\AccessControl;
+use yii\filters\VerbFilter;
 
 class DashboardController extends Controller
 {
+    public function behaviors()
+    {
+        return [
+            'access' => [
+                'class' => AccessControl::className(),
+                'rules' => [
+                    [
+                        'actions' => ['login', 'error'],
+                        'allow' => true,
+                    ],
+                    [
+                        'actions' => ['logout', 'index'],
+                        'allow' => true,
+                        'roles' => ['@'],
+                    ],
+                ],
+            ],
+            'verbs' => [
+                'class' => VerbFilter::className(),
+                'actions' => [
+                    'logout' => ['post'],
+                ],
+            ],
+        ];
+    }
+
     public function actionIndex()
     {
         return $this->render('index');
